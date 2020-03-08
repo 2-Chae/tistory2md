80a3bc527c3e92ccc302bc2e88ea7ad3ba94bbc7
Chaehyeon Lee <123456ccdd@naver.com>
2020-03-08 18:43:24 +0900

    add backup.py

---
+++ backup.py
@@ -0,0 +1,148 @@
+import requests
+import json
+import html2text
+import os
+import errno
+import re
+from io import StringIO
+import urllib
+
+import ssl
+
+def create_folder(filename):
+    if not os.path.exists(os.path.dirname(filename)):
+            try:
+                os.makedirs(os.path.dirname(filename))
+            except OSError as exc: # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+
+class BackUp():
+    def __init__(self, blogName, token, dir):
+        ssl._create_default_https_context = ssl._create_unverified_context
+        self.blogName = blogName
+        self.token = token
+        self.dir = dir
+
+        self.h = html2text.HTML2Text()
+        self.h.ignore_links = True
+
+
+
+    def start_backup(self, i):
+        headers = {'Content-Type': 'application/json;'} #charset=utf-8'} 
+        url = "https://www.tistory.com/apis/post/read"
+        parms = {'access_token': self.token,
+                'blogName': self.blogName,
+                'postId': i,
+                'output': 'json'}
+        response = requests.get(url, headers=headers, params = parms)
+        return response
+
+def save_document(self, r):
+    link_list = []
+    ref_list = []
+    catid   = r['tistory']['item']['categoryId']
+    docid   = r['tistory']['item']['id']
+    date    = r['tistory']['item']['date']
+    title   = r['tistory']['item']['title']
+    content = r['tistory']['item']['content']
+        
+    if not catid:
+        catid = 'no_category'
+
+    cat = catid
+    # if catid in cats:
+    #     cat = cats[catid][0]
+    
+        
+    folder = myblog + '/' + cat + '/' + docid
+    filename = folder + '/' + date.split(' ')[0] + '-' + title + '.md'
+    create_folder(filename)
+
+    try:
+        final = h.handle(content)        
+        buf = final.split('\n')
+        
+        for k, line in enumerate(buf):
+            if 'https://t1.daumcdn.net/tistory_admin/assets/' in line or 'http://kage.tistory.com/image/' in line:
+                continue
+            
+            version = 0
+            if '[##_Image' in line: # 최신버전 이미지  서버
+                links = [line]
+                version = 1
+            elif 'cfile' in line or 'data:image' in line or 'k.kakaocdn.net' in line or 'tutorialspoint' in line:
+                links = re.findall(r'!\[\]\((.*?)\)', line)
+                version = 2
+            else: # 구버전
+                links = re.findall(r'!\[\]\((.*?)\)', line)
+
+            newline = ''
+            for l in links:               
+                if ('img.png' not in l) and ('image' not in l) and ('Image' not in l):
+                    continue
+
+                print(l)
+
+                if version == 1:
+                    temp = l.split('@')[1]
+                    temp = temp.split('|')[0]
+                    if 'img' in temp:
+                        l = 'https://k.kakaocdn.net/dn/' + temp
+                    else:
+                        l = 'https://t1.daumcdn.net/cfile/tistory/' + temp
+                elif version == 2:
+                    l = l
+                else:
+                    l = 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https://k.kakaocdn.net/dn/' + l.split('@')[1]
+                    
+                print(l)
+                link_list.append(l)
+                i = len(ref_list)
+                ref = '[link{}]:{}'.format(i, l)
+                newline += '![][link{}]'.format(i)
+                ref_list.append(ref)
+                
+            if len(links) > 0:
+                buf[k] = newline
+        
+        buf.extend(ref_list)
+
+        with open(filename, 'w') as f:
+            f.write('---' + '\n')
+            f.write('layout: post' + '\n')
+            f.write('title: \'' + title + '\'\n')
+            if catid in cats:
+                cat = cats[catid][1]
+                tag = cats[catid][0]
+                f.write('tags: \'' + tag + '\'\n')
+            f.write('categories: \'' + cat + '\'\n')
+            f.write('permalink: /category/'+ cat.lower() + '/' + docid + '\n')
+            f.write('---' + '\n')
+
+
+            for line in buf:
+                # Responsive embedded youtube 넣기
+                if 'https://youtu.be/' in line:
+                    f.write('{% youtube '+ line +' %}')
+
+                 # invalid-file 제거하기 or 거르고 싶은 문자열 제거.
+                elif ('data-origin-width' in line) or ('invalid-file' in line) or ('---|---' in line) or ('|' in line) or ('https://t1.daumcdn.net/tistory_admin/assets/' in line) or ('origin-width' in line):
+                    continue
+                else:
+                    f.write((str(line) + '\n'))
+
+        # print(link_list)
+        if download_img:
+            for k, link in enumerate(link_list):
+                fillname = folder + '/' + str(k) + '.jpg'
+                create_folder(fillname)
+
+                try:
+                    urllib.request.urlretrieve(link, fillname)
+                except:
+                    print('failed to download image :', link)
+
+    except UnicodeEncodeError as e:
+        print(e)
--- tistory2md.py
+++ tistory2md.py
@@ -1,19 +1,75 @@
 # -*- coding: utf-8 -*-
 import sys
-from PyQt5 import QtCore, QtGui
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
 from tistory2md_ui import Ui_Dialog
+from backup import BackUp
 
 class XDialog(QDialog, Ui_Dialog):
     def __init__(self):
         QDialog.__init__(self)
         # setupUi() 메서드는 화면에 다이얼로그 보여줌
         self.setupUi(self)
-        self.startBtn.clicked.connect(self.sayHello)
+        self.startBtn.clicked.connect(self.start)
+        self.error_dialog = QtWidgets.QErrorMessage()
+        self.saveDir.setText(os.getcwd())
+    def start(self):
+        self.status.clear()
+        if self.inputValidation() == False:
+            return
+
+        backup = BackUp(self.blogName.text(), self.accessToken.text(), self.saveDir.text())
+
+        for i in range(int(self.startNum.text()), int(self.endNum.text())+1):    
+            response = backup.start_backup(i)
+
+            if response.status_code == 200:
+                continue
+            else:
+                self.status.append('[Error #'+str(i)+']\n'+response.text)
 
 
-    def sayHello(self):
-        self.saveDir.setText('Hello world!')
+    def inputValidation(self):
+        
+        
+        # isEmpty check
+        if self.blogName.text() == "":
+            self.error_dialog.showMessage('블로그 주소를 입력해주세요.')
+            self.error_dialog.exec_()
+            return False
+        if self.accessToken.text() == "":
+            self.error_dialog.showMessage('Access token을 입력해주세요.')
+            self.error_dialog.exec_()
+            return False
+        if self.startNum.text() == "" or self.endNum.text() == "":
+            self.error_dialog.showMessage('문서 번호를 입력해주세요.')
+            self.error_dialog.exec_()
+            return False
+        
+        # valid check
+        if self.startNum.text() != "":
+            try:
+                int(self.startNum.text())
+            except ValueError:
+                self.error_dialog.showMessage('문서 번호는 숫자만 넣어주세요.')
+                self.error_dialog.exec_()
+                return False
+        if self.endNum.text() != "":
+            try:
+                int(self.endNum.text())
+            except ValueError:
+                self.error_dialog.showMessage('문서 번호는 숫자만 넣어주세요.')
+                self.error_dialog.exec_()
+                return False
+
+        if int(self.startNum.text()) > int(self.endNum.text()):
+            self.error_dialog.showMessage('문서 시작번호는 끝번호보다 작은 수여야 합니다.')
+            self.error_dialog.exec_()
+            return False
+
+
+        return True
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
